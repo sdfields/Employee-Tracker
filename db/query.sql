021a36f9b2a52391da5ef36f78ac215df0c36a3a
sdfields <spencerdfields@gmail.com>
2023-03-09 04:24:43 -0600

    added to do list in query sql in db

---
+++ db/query.sql
@@ -0,0 +1,28 @@
+/* List of query */
+
+/* Add Employee */
+
+
+
+/* View Employees */
+
+
+
+/* Add Role */
+
+
+
+/* View Roles */
+
+
+
+/* Update Role */
+
+
+
+/* Add Department */
+
+
+
+/* View Departments */
+
