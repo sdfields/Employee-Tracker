0a0af636928cdb15b4e4969c45b0a270d798a9bf
sdfields <spencerdfields@gmail.com>
2023-03-09 03:56:08 -0600

    created WIP switch case need to test

--- index.js
+++ index.js
@@ -14,7 +14,36 @@ function track() {
         choices: ['Add Employee', 'View Employees', 'Add Role', 'View Roles', 'Update Role', 'Add Department', 'View Departments']
         },
     ])
-
+    .then((choice) => {
+        const userSelect = choice
+        switch (userSelect) {
+        case 'Add Employee':
+            addEmployee()
+            break;
+        case 'View Employees':
+            viewEmployees()
+            break;
+        case 'Add Role'
+            addRole()
+            break;
+        case 'View Roles'
+            addRole()
+            break;
+        case 'Update Role'
+            addRole()
+            break;
+        case 'Add Deparment'
+            addRole()
+            break;
+        case 'View Departments'
+            addRole()
+            break;
+        }
+    })
 }
 
 track();
+
+addEmployee() {
+    console.log('Employee Added!')
+}
